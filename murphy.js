afd321c1d9a741c80ce91b862bd20892578210bd
Iain Dooley <iain@workingsoftware.com.au>
2018-03-09 12:39:01 +1100

    Pass through command line arguments to executing sub processes

--- murphy.js
+++ murphy.js
@@ -1,17 +1,24 @@
-const path = require("path");
-const fs = require("fs");
-const glob = require("glob")
-const {exec} = require('child_process');
-const cmd = "node ";
+const path     = require("path");
+const fs       = require("fs");
+const glob     = require("glob")
+const {exec}   = require('child_process');
+const cmd      = "node ";
+var   cmd_args = "";
 
 module.exports = {
     run: function(dirname)
     {
-        glob(dirname+"/*.js.murphy/*.js",function(er,files)
+        if(process.argv.length > 2)
+        {
+            for(var i = 2;i < process.argv.length;i++)
+                cmd_args += " "+process.argv[i];
+        }
+    
+        glob(dirname+"/**/*.js.murphy/*.js",function(er,files)
         {
             for(var i = 0;i < files.length;i++)
             {
-                exec(cmd+" "+files[i], (err, stdout, stderr) =>
+                exec(cmd+" "+files[i]+cmd_args, (err, stdout, stderr) =>
                 {
                     if (err)
                         console.log("Unable to execute: "+cmd);
